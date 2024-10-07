4d8baba56793246a23893d8eb1c9d9c1b01e6120
chi@chi.com <chi@chi.com>
2024-10-07 19:13:53 +0100

    Trying to fix put

--- products/serializers.py
+++ products/serializers.py
@@ -38,3 +38,27 @@ class ProductSerializer(serializers.ModelSerializer):
             ProductImage.objects.create(product=product, **image_data)
 
         return product
+    
+    def update(self, instance, validated_data):
+        # Extract nested category and images data
+        category_data = validated_data.pop('category')
+        images_data = validated_data.pop('images', [])
+
+        # Update the category (assuming categories are not created through PUT)
+        category, created = Category.objects.get_or_create(**category_data)
+        instance.category = category
+
+        # Update the product fields
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.price = validated_data.get('price', instance.price)
+        instance.stock_quantity = validated_data.get('stock_quantity', instance.stock_quantity)
+        instance.save()
+
+        # Clear existing images and add new ones
+        instance.images.clear()  # Or delete and recreate images if necessary
+        for image_data in images_data:
+            ProductImage.objects.create(product=instance, **image_data)
+
+        return instance
+
